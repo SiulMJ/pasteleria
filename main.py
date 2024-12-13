2893c52a5a7a71e9348a0572b710d086b5979920
Siulmj <luis.esgrima02@gmail.com>
2024-12-12 23:23:53 -0600

    primer commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="template")
+
+app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run(host="localhost", port=4000, debug=True)
