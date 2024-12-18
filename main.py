531fed451ad26d54f6a965d550d9bfbd90f4d9f6
Siulmj <luis.esgrima02@gmail.com>
2024-12-17 22:30:52 -0600

    add || structure

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder="template")
+app = Flask(__name__, template_folder="templates")
 
-app.route("/")
+@app.route("/")
 def index():
     return render_template("index.html")
 
